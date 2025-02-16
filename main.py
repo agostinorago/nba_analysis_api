745e7a9edc3c3f2db54b77628a99be9c9f654c92
agostinorago <ragoagostino10@gmail.com>
2025-02-16 17:47:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseSettings
+import os
+
+class Settings(BaseSettings):
+    database_url: str = os.getenv("DATABASE_URL")
+    secret_key: str = os.getenv("SECRET_KEY")
+    api_key: str = os.getenv("API_KEY")
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Hello": "World",
+        "Database URL": settings.database_url,
+        "API Key": settings.api_key
+    }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
