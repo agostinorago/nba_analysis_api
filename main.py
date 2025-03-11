1fef306cd51af0492ded38fb88f0e606e07d8820
agostinorago <ragoagostino10@gmail.com>
2025-03-11 15:09:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from pydantic_settings import BaseSettings
 # Configura il logging
 logging.basicConfig(level=logging.INFO)
 
-# Classe per caricare le configurazioni
+# Classe per caricare le configurazioni dal file .env
 class Settings(BaseSettings):
-    database_url: str = "postgresql://gioiellino_admin:LnRlKuDdE6FGiN3fuWi0L1uWhYC7Oqep@dpg-cup2amaj1k6c739f45p0-a.frankfurt-postgres.render.com/gioiello_db"
-    api_key: str = "6fb9923350bef0e66e7f17d0420511e7"
+    database_url: str
+    api_key: str
 
     class Config:
         env_file = ".env"  # Carica le variabili d'ambiente dal file .env
@@ -18,20 +18,8 @@ class Settings(BaseSettings):
 # Inizializza le configurazioni
 settings = Settings()
 
-# Log delle configurazioni
-logging.info(f"DATABASE_URL: {settings.database_url}")
-logging.info(f"API_KEY: {settings.api_key}")
+# Log (senza stampare dati sensibili)
+logging.info("Configurazione caricata con successo!")
 
 # Crea l'app FastAPI
-app = FastAPI(title="La mia App su Render")
-
-@app.get("/")
-def read_root():
-    return {
-        "API Key": settings.api_key,
-    }
-
-# Avvio dell'app con Uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI(title="La mia A
